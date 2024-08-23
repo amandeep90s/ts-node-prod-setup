a959bd6aea2dcb47032adb56ff1a4a6789b40f88
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-23 21:08:23 +0530

    feat: Winston file logger setup

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,6 +1,7 @@
+import path from 'path';
 import util from 'util';
 import { createLogger, format, transports } from 'winston';
-import { ConsoleTransportInstance } from 'winston/lib/winston/transports';
+import { ConsoleTransportInstance, FileTransportInstance } from 'winston/lib/winston/transports';
 import config from '../config/config';
 import { EApplicationEnvironment } from '../constant/application';
 
@@ -32,9 +33,47 @@ const consoleTransport = (): Array<ConsoleTransportInstance> => {
   return [];
 };
 
+const fileLogFormat = format.printf((info) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { level, message, timestamp, meta = {} } = info;
+
+  const logMeta: Record<string, unknown> = {};
+
+  for (const [key, value] of Object.entries(meta as Record<string, unknown>)) {
+    if (value instanceof Error) {
+      logMeta[key] = {
+        name: value.name,
+        message: value.message,
+        trace: value.stack ?? ''
+      };
+    } else {
+      logMeta[key] = value;
+    }
+  }
+
+  const logData = {
+    level: level.toUpperCase(),
+    message: (message as string) ?? '',
+    timestamp: (timestamp as string) ?? '',
+    meta: logMeta
+  };
+
+  return JSON.stringify(logData, null, 4);
+});
+
+const fileTransport = (): Array<FileTransportInstance> => {
+  return [
+    new transports.File({
+      filename: path.join(__dirname, '../', '../', 'logs', `${config.ENV}.log`),
+      level: 'info',
+      format: format.combine(format.timestamp(), fileLogFormat)
+    })
+  ];
+};
+
 export default createLogger({
   defaultMeta: {
     meta: {}
   },
-  transports: [...consoleTransport()]
+  transports: [...fileTransport(), ...consoleTransport()]
 });
