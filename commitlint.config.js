25c11f8c10d6e25689c94b23fdfec60e9c04674c
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-23 09:59:48 +0530

    feat: Commitlint setup

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['@commitlint/cli', '@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', // new feature
+        'fix', // bug fix
+        'docs', // documentation
+        'style', // formatting, missing semi colons, etc; no code change
+        'refactor', // refactoring production code
+        'perf', // performance improvement
+        'test', // adding missing tests, refactoring tests; no production code change
+        'build', // build system or external dependencies
+        'ci', // CI related changes
+        'chore', // updating grunt tasks etc; no production code change
+        'revert', // reverting changes
+      ],
+    ],
+    'subject-case': [2, 'always', 'sentence-case'], // sentence-case is the default value
+  },
+};
