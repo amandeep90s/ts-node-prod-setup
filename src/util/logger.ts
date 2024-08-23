e4e0690d5fe892dcee46abfdb56cd5fcfafb1714
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-23 22:04:46 +0530

    feat: Database log storage

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -3,6 +3,8 @@ import path from 'path';
 import * as sourceMapSupport from 'source-map-support';
 import util from 'util';
 import { createLogger, format, transports } from 'winston';
+import 'winston-mongodb';
+import { MongoDBTransportInstance } from 'winston-mongodb';
 import { ConsoleTransportInstance, FileTransportInstance } from 'winston/lib/winston/transports';
 import config from '../config/config';
 import { EApplicationEnvironment } from '../constant/application';
@@ -90,9 +92,24 @@ const fileTransport = (): Array<FileTransportInstance> => {
   ];
 };
 
+const mongodbTransport = (): Array<MongoDBTransportInstance> => {
+  return [
+    new transports.MongoDB({
+      level: 'info',
+      db: config.DATABASE_URL as string,
+      metaKey: 'meta',
+      expireAfterSeconds: 3600 * 24 * 30,
+      options: {
+        useUnifiedTopology: true
+      },
+      collection: 'application-logs'
+    })
+  ];
+};
+
 export default createLogger({
   defaultMeta: {
     meta: {}
   },
-  transports: [...fileTransport(), ...consoleTransport()]
+  transports: [...fileTransport(), ...mongodbTransport(), ...consoleTransport()]
 });
